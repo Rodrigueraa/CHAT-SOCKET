5e6f57502863d0dcef48383732cb5784b12befe5
rodrigueraa <rodrigocaua@alu.ufc.br>
2023-09-14 15:04:57 -0300

    up

--- client.py
+++ client.py
@@ -1,23 +1,35 @@
-#cliente
-
 from socket import *
+import threading
 
 host = gethostname()
 port = 12000
 
+def receive(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode('utf-8')
+            if not msg:
+              break
+            print(msg)
+        except ConnectionResetError:
+            break
+
+def send(client_socket):
+    while True:
+        msg = input()
+        if msg == 'tt':
+            client_socket.send(msg.encode('utf-8'))
+            break
+        client_socket.send(msg.encode('utf-8'))
+
+
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((host, port))
 print('CLIENTE: ')
-terminado = False
-print('Digite tt para terminar o chat')
 
-while not terminado:
-    client.send(input('You: ').encode('utf-8'))
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'tt':
-        terminado = True
-        client.close()
-    else: 
-        print('Stranger: ', msg)
+recv_thread = threading.Thread(target=receive, args=(client,))
+recv_thread.start()
 
-client.close()
+send_thread = threading.Thread(target=send, args=(client,))
+send_thread.start()
+
--- server.py
+++ server.py
@@ -1,25 +1,43 @@
 from socket import *
+import threading
 
 host = gethostname()
 port = 12000
 
 print(f'INFORMAÇOES DO SERVIDOR-> \nHOST: {host}, PORT {port} \n')
 
+def receive(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode('utf-8')
+            if not msg:
+              break
+            print(msg)
+        except ConnectionResetError:
+            break
+
+def send(client_socket):
+    while True:
+        msg = input()
+        if msg == 'tt':
+            client_socket.send(msg.encode('utf-8'))
+            break
+        client_socket.send(msg.encode('utf-8'))
+
+
 server = socket(AF_INET, SOCK_STREAM)
 server.bind(('', port))
 server.listen()
-client, end = server.accept()
 
-terminado = False
-while not terminado:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'tt':
-        terminado = True
-        server.close()
-    else:
-        print('Stranger: ', msg)
-        client.send(input('You: ').encode('utf-8'))
+while 1:
+    client_socket, client_address = server.accept()
+    print(f'Conectado com {client_address}')
+    recv_thread = threading.Thread(target=receive, args=(client_socket,))
+    recv_thread.start()
 
-client.close()
+    send_thread = threading.Thread(target=send, args=(client_socket,))
+    send_thread.start()
+
+client_socket.close()
 server.close()
 
